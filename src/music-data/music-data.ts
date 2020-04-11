125a47800a3f0be5e125be236048eb7efb2499f6
Evaldas Latoskinas <evaldasl61@gmail.com>
2020-04-11 20:51:19 +0200

    Implement music-data classes based on UML diagrams

--- src/music-data/music-data.ts
+++ src/music-data/music-data.ts
@@ -4,6 +4,78 @@
 
 import { IPicture } from 'music-metadata'
 
+/**
+ * Metadata Classes
+ */
+
+/**
+ * Class to represent the data of a generic Music Entry;
+ * Contains common data for music entries (songs & albums)
+ */
+abstract class MusicEntryData {
+    title: string
+    year: number
+    duration: number
+    genres: string[]
+    covers: IPicture[]
+}
+
+/**
+ * Class to represent the data of a Song.
+ * Contains all the necessary song information,
+ * as well as the album that the song is linked to.
+ */
+class SongData extends MusicEntryData {
+    artists: string[] = []
+
+    album: AlbumData
+    track: number
+    disk: number
+
+    rating: number
+}
+
+/**
+ * Class to represent the data of an Album.
+ * Contains all the necessary album information,
+ * but does not contain the information of each concrete song.
+ * TODO: Add individual song data as well?
+ */
+class AlbumData extends MusicEntryData {
+    artist: string
+    totalTracks: number
+    totalDisks: number
+}
+
+/**
+ * Music Entry classes
+ */
+
+/**
+ * Generic Music Entry class definition
+ */
+class MusicEntry<D extends MusicEntryData> {
+    data: D
+}
+
+/**
+ * Class corresponding to a song, containing the concrete
+ * sound object, as well as the song data.
+ * 
+ * TODO: Rename to Song after migrating from original Song
+ */
+class Song_ extends MusicEntry<SongData> {
+    sound: Howl
+}
+
+/**
+ * Class corresponding to an album, containing a list of
+ * songs that the album contains in order, as well as the
+ * album data.
+ */
+class Album extends MusicEntry<AlbumData> {
+    songs: Song[]
+}
 
 /**
  * Container for a single song, containing all the neccessary metadata info.

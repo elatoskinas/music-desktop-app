6d55f0577b96838ab0931a8cf44a55207c87a1ca
Evaldas Latoskinas <evaldasl61@gmail.com>
2020-11-01 02:16:15 +0100

    Split time into separate text boxes

--- src/frontend/music-progress.styles.ts
+++ src/frontend/music-progress.styles.ts
@@ -44,3 +44,8 @@ export const StyledProgressBar = styled.input`
         background-color: ${progressFilledColor};
     }
 `
+
+export const StyledProgressTimeContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
